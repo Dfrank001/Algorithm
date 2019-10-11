39d2c9eab2f55eba1d27d98ae7443bdefb6b4ce8
Dfrank001 <frank226969@qq.com>
2019-10-12 01:10:39 +0800

    one

---
+++ JavaAlgorithm/src/fileIO/FileIO.java
@@ -0,0 +1,14 @@
+package fileIO;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class FileIO {
+    private File file;
+    public FileIO(File file) {
+        this.file=file;
+    }
+    public ArrayList arrList(){
+
+    }
+}
